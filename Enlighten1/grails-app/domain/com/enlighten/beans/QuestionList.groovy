6695ee0d138b660f467a1d68fdca5d6c667bebfb
rubyag <ruby.agrawal27@gmail.com>
2014-06-17 21:45:50 +0530

    student

---
+++ Enlighten1/grails-app/domain/com/enlighten/beans/QuestionList.groovy
@@ -0,0 +1,9 @@
+package com.enlighten.beans
+/**
+ * Created by hp on 6/17/2014.
+ */
+class QuestionList {
+    String subjectName;
+    Set<Question> questionSet;
+
+}
---
+++ Enlighten1/grails-app/domain/com/enlighten/beans/Subjects.groovy
@@ -0,0 +1,11 @@
+package com.enlighten.beans
+
+/**
+ * Created by hp on 6/17/2014.
+ */
+class Subjects {
+    private String subjectName[]={"C", "C++","Java","HTML","MySQL",
+        "MSOffice","Tally","Busy","Photoshop","CoralDraw","PageMaker"
+    };
+
+}
